38e1934c42cd3e4a03b918d419b3c2fb8f5f695d
Pranith Kumar <bobby.prani@gmail.com>
2016-11-05 13:13:44 -0400

    Run background processes

--- src/main.rs
+++ src/main.rs
@@ -5,10 +5,27 @@ use std::process::Command;
 fn process(buffer: &String) {
 
     let mut tokens: Vec<&str> = buffer.split_whitespace().collect();
+    let buffer_len = tokens.len();
+
+    if buffer_len == 0 {
+        return;
+    }
+
+    let no_wait = tokens[buffer_len - 1] == "&";
+    let mut args = tokens.split_off(1);
+
+    if no_wait {
+        args.truncate(buffer_len - 2);
+    }
+
     let mut subproc = Command::new(tokens[0]);
-    let mut subproc_with_args = subproc.args(&tokens.split_off(1));
+    let mut subproc_with_args = subproc.args(&args);
     let mut child = subproc_with_args.spawn().expect("Failed to execute");
-    let _ = child.wait();
+
+    if no_wait == false {
+        let _ = child.wait();
+    }
+
     let _ = io::stdout().flush();
 }
 
